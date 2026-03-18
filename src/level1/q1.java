235507f8772b27f12bd34c96ac66caddf27310e0
harshini1920 <harshinisubramanian1901@gmail.com>
2026-03-18 09:29:11 +0530

    [Harshini] added: Java code for Birth Year

---
+++ src/level1/q1.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class q1 {
+    public static void main(String [] args){
+        int a=2000;
+        int b=2024;
+        int c=b-a;
+        System.out.println("Harry's age in 2024 is "+c);
+    }
+}
